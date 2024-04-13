f67905c6b2a31c06a6f8a975ef620e00294a7433
prammmoe <ikhwanpramuditha05@gmail.com>
2024-04-13 19:55:49 +0700

    add post movie

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const movieController = require("../controllers/movieController");
 
 router.get("/movies", movieController.getMovie);
+router.post("/movies", movieController.addMovie);
 
 module.exports = router;
