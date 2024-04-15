0aaf7d33091f32592a4551c580cad2911151b6fd
prammmoe <ikhwanpramuditha05@gmail.com>
2024-04-15 16:25:11 +0700

    change movie controller

--- src/controllers/genreController.js
+++ src/controllers/genreController.js
@@ -1,16 +1,11 @@
 const db = require("../configs/db"); // Assuming this imports a database connection
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const getGenre = (req, res) => {
-  db.query("SELECT * FROM genres", function (err, results) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.status(200).json({
-        message: "GET all genres success",
-        data: results,
-      });
-    }
-  });
+const getGenre = async (req, res) => {
+  const genre = await prisma.genre.findMany();
+
+  res.send(genre);
 };
 
 module.exports = { getGenre };
